51059c965c48e9d4a53dc7b2915edec4f5528b50
Caleb <csh246@cornell.edu>
2026-04-24 00:31:03 -0400

    render tests

--- lib/render.ml
+++ lib/render.ml
@@ -34,6 +34,28 @@ let render_board _board =
   print_endline
     ("Number of walls remaining: " ^ string_of_int _board.walls_remaining)
 
+let str_render_board board =
+  let buf = Buffer.create 128 in
+
+  let tile_to_string = function
+    | Camel -> "C "
+    | Water -> "W "
+    | Wall -> "B "
+    | Blank -> "G "
+  in
+
+  Array.iter
+    (fun row ->
+      Array.iter (fun tile -> Buffer.add_string buf (tile_to_string tile)) row;
+      Buffer.add_char buf '\n')
+    board.grid;
+
+  Buffer.add_string buf "Coordinates of the Camel are (_, _)\n";
+  Buffer.add_string buf
+    ("Number of walls remaining: " ^ string_of_int board.walls_remaining);
+
+  Buffer.contents buf
+
 let print_place_result _result =
   match _result with
   | Out_of_bounds -> print_endline "Coordinates out of bounds! Try again"
---
+++ test/test_enclosecamel.ml
@@ -0,0 +1,44 @@
+open OUnit2
+open EncloseCamel
+open Model
+open Render
+
+let test_render_board _ =
+  let board1 =
+    {
+      grid = [| [| Camel; Blank |]; [| Blank; Water |] |];
+      walls_remaining = 3;
+    }
+  in
+  let result = str_render_board board1 in
+  let expected =
+    "C G \n\
+     G W \n\
+     Coordinates of the Camel are (_, _)\n\
+     Number of walls remaining: 3"
+  in
+  assert_equal expected result
+
+  let test_render_empty _ =
+  let board =
+    {
+      grid = [| [| |]|];
+      walls_remaining = 3;
+    }
+  in
+  let result = str_render_board board in
+  let expected =
+    "\n\
+     Coordinates of the Camel are (_, _)\n\
+     Number of walls remaining: 3"
+  in
+  assert_equal expected result
+
+let tests =
+  "test suite"
+  >::: [
+        "render_board basic" >:: test_render_board;
+         "render_board empty" >:: test_render_empty
+       ]
+
+let _ = run_test_tt_main tests
